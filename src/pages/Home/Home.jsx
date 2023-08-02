4086cb7b89846d28946b9dd5b88f825f78df4f68
Chrapson <marcin.chraplewski@gmail.com>
2023-08-03 00:00:02 +0200

    [Phonebook][chore] add styles to hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
-import hero from '../../images/hero.jpg';
 
 export const HomePage = () => {
   return (
@@ -10,7 +9,6 @@ export const HomePage = () => {
         <title>Phonebook</title>
       </Helmet>
       <div className={styles.container}>
-        {/* <img src={hero} alt="phonebook" width="70" height="50"></img> */}
         <h1 className={styles.title}>Hello,</h1>
         <h2 className={styles.subtitle}>Welcome to Phonebook App</h2>
         <p className={styles.info}>
